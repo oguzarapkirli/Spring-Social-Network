93f784950dcc643151371c68b43667049d490050
Oğuz Arapkirli <oguzarapkirli06@gmail.com>
2023-02-20 00:09:25 +0300

    Initial Commit

---
+++ src/main/java/com/example/springsocialnetwork/SpringSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsocialnetwork/SpringSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
